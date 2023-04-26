237eecff04aef4230699cc577f266ecb17f23b0a
Justin Lucero <justinluceroxd04@gmail.com>
2023-04-26 10:42:36 -0500

    Programacion de las clases del UML

--- src/main/java/ec/edu/ups/java/practica/dos/main/Principal.java
+++ src/main/java/ec/edu/ups/java/practica/dos/main/Principal.java
@@ -4,6 +4,10 @@
 
 package ec.edu.ups.java.practica.dos.main;
 
+import ec.edu.ups.java.practica.dos.clases.Cliente;
+import ec.edu.ups.java.practica.dos.clases.Directivo;
+import ec.edu.ups.java.practica.dos.clases.Empresa;
+
 /**
  *
  * @author Usuario
@@ -11,6 +15,19 @@ package ec.edu.ups.java.practica.dos.main;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Cliente cliente = new Cliente("094984000","0151156", "PEPITO", "CALLE VIEJA","PEPITO@ADHSJSHAJDKSA");
+        
+        Empresa empresaUno = new Empresa("051656156000", "salesianos . in");
+        empresaUno.agregarEmpleado(1000.26, "fghgh02", "Juan", "calle nueva", "juanfjhajfhjkaf");
+        empresaUno.agregarEmpleado(625.26, "fghgh03", "diego", "calle nueva 1", "diegofjhajfhjkaf");
+        empresaUno.agregarEmpleado(585.65, "fghgh25", "carlos", "calle nueva 2", "carlosjhajfhjkaf");
+        
+        
+        empresaUno.agregarDirectivo("jefe", 2000, "006550", "jose", "direccion 6", "josejkdsajdjskla");
+        
+        empresaUno.agrgarSubordinadoDitectivo("fghgh02", "006550");
+        
+        System.out.println("Datos empresa: "+ empresaUno.toString());
+        
     }
 }
